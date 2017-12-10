bf245a37cfff4220ed3dc6f4eb170a8c67403083
Console Argonaut <vanderheide.michael@gmail.com>
2017-12-10 17:25:01 +0100

    Added Page creation of  content

--- src/System/loadPage.php
+++ src/System/loadPage.php
@@ -1,5 +1,11 @@
 <?php
 
-include("Styling/index.html");
+$elm_Page_Content = '<h1>404 Page not found</h1>';
+
+$elm_Page_HTML = file_get_contents('Styling/index.html', FILE_USE_INCLUDE_PATH);
+
+$elm_Page_HTML = str_replace("[elm_PageContent]", $elm_Page_Content, $elm_Page_HTML);
+
+eval('?>'. $elm_Page_HTML . '<?php');
 
 ?>
