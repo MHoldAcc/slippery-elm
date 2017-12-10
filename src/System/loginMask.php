46413a90278dbab05f9a7329d13f155215006c2c
LYNDWYN\Laura <laura.steiner@nyp.ch>
2017-12-10 17:21:17 +0100

    Login Mask added

---
+++ src/System/loginMask.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Laura
+ * Date: 05.12.2017
+ * Time: 18:30
+ */
+?>
+
+<form action="login.php" method="post">
+    Username: <input type="text" name="username" /><br />
+			Passwort: <input type="password" name="password" /><br />
+		<input type="submit" value="Anmelden" />
+</form>
