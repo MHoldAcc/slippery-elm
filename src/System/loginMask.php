1d1f7636205f078a7d6d8d227d6a8c1ad2d2ced8
Console Argonaut <vanderheide.michael@gmail.com>
2017-12-10 17:32:28 +0100

    Login mask gets loaded correctly into page content

--- src/System/loginMask.php
+++ src/System/loginMask.php
@@ -7,7 +7,7 @@
  */
 ?>
 
-<form action="login.php" method="post">
+<form action="index.php" method="post">
     Username: <input type="text" name="username" /><br />
 			Passwort: <input type="password" name="password" /><br />
 		<input type="submit" value="Anmelden" />
