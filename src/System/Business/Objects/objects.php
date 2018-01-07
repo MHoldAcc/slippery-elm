c518e5bdb9f1301ff63c71c6547ebaf1cde91db4
Console Argonaut <vanderheide.michael@gmail.com>
2018-01-07 18:32:05 +0100

    Added elm_page object

--- src/System/Business/Objects/objects.php
+++ src/System/Business/Objects/objects.php
@@ -8,4 +8,17 @@ class elm_User {
     public $roleId;
 }
 
+class elm_Page {
+    public $id;
+    public $name;
+    public $content;
+    public $parentPage;
+    public $keywords;
+    public $sorting;
+    public $created;
+    public $modified;
+    public $creatorId;
+    public $modifierId;
+}
+
 ?>
