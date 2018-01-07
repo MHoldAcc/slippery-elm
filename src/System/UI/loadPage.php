390f72151b9a9dad2886b35002b5854465f86f74
Console Argonaut <vanderheide.michael@gmail.com>
2018-01-07 16:52:54 +0100

    Added Login functionality

--- src/System/UI/loadPage.php
+++ src/System/UI/loadPage.php
@@ -1,24 +1,45 @@
 <?php
+include_once("System/Business/Login/login.php");
 
 function elm_Page_Load(){
+    global  $elm_Page_HTML;
+    global  $elm_Page_Content;
+
     $elm_Page_HTML = file_get_contents('Styling/index.html', FILE_USE_INCLUDE_PATH);
 
     $elm_Page_Content = '<h1>404 Page not found</h1>';
 
-    if(isset($_GET['login']))
-        $elm_Page_Content = file_get_contents('System/UI/HTML/loginMask.html', FILE_USE_INCLUDE_PATH);
+    elm_Page_LoginFunctionality();
 
-    $elm_Page_HTML = elm_Page_ReplacePlaceholder($elm_Page_HTML, "[elm_PageContent]", $elm_Page_Content);
-
-    $elm_Page_HTML = elm_Page_ReplacePlaceholder($elm_Page_HTML, "[elm_Login_Link]", "index.php?login");
-    $elm_Page_HTML = elm_Page_ReplacePlaceholder($elm_Page_HTML, "[elm_Login_Text]", "Anmelden");
+    elm_Page_ReplacePlaceholder("[elm_PageContent]", $elm_Page_Content);
 
     eval('?>'. $elm_Page_HTML . '<?php');
 }
 
-function elm_Page_ReplacePlaceholder($pagecontent, $placeholder, $value){
-    $pagecontent = str_replace("$placeholder", $value, $pagecontent);
-    return $pagecontent;
+function elm_Page_ReplacePlaceholder($placeholder, $value){
+    global  $elm_Page_HTML;
+    $elm_Page_HTML = str_replace($placeholder, $value, $elm_Page_HTML);
+}
+
+function elm_Page_LoginFunctionality(){
+    global  $elm_Page_Content;
+
+    //Login functionality
+    if(isset($_GET['login']))
+        $elm_Page_Content = file_get_contents('System/UI/HTML/loginMask.html', FILE_USE_INCLUDE_PATH);
+    if(isset($_GET['logout']))
+        elm_Login_Logout();
+    if(elm_Login_IsLoggedIn()){
+        elm_Page_ReplacePlaceholder("[elm_Login_Link]", "index.php?logout");
+        elm_Page_ReplacePlaceholder("[elm_Login_Text]", "Abmelden");
+    }
+    else{
+        elm_Page_ReplacePlaceholder("[elm_Login_Link]", "index.php?login");
+        elm_Page_ReplacePlaceholder("[elm_Login_Text]", "Anmelden");
+        if(isset($_POST['elm_Login_Execute'])){
+            elm_Login_Login($_POST['elm_Login_Username'], $_POST['elm_Login_Password'], $_POST['elm_Login_Password']);
+        }
+    }
 }
 
 ?>
