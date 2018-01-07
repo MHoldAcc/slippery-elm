d94b45cdbc55b935f0dcf7b84510d0f8ee8bdac2
Console Argonaut <vanderheide.michael@gmail.com>
2018-01-07 17:38:59 +0100

    Fixed redirect error

--- src/System/Business/Login/login.php
+++ src/System/Business/Login/login.php
@@ -3,7 +3,7 @@
 function elm_Login_Login($userName, $password, $verify){
     if(isset($userName) && isset($password) && isset($verify)){
         elm_Data_login_User($userName, $password, $verify);
-        header("Location: ../index.php");
+        header("Location: index.php");
     }
     else{
         //Login failed
@@ -15,11 +15,11 @@ function elm_Login_Logout(){
     session_start();
     session_unset();
     session_destroy();
-    header("Location: ../index.php");
+    header("Location: index.php");
 }
 
 function elm_Login_IsLoggedIn(){
-    if(isset($_SESSION['login_user']) && isset($_SESSION['login_failure']) && $_SESSION['login_failure'] == 'false')
+    if(isset($_SESSION['login_user']))
         return true;
     else
         return false;
