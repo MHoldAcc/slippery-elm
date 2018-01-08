95088dfe613b90076874e152e0d58ef823c02fe1
Console Argonaut <vanderheide.michael@gmail.com>
2018-01-08 17:25:19 +0100

    Added empty Admin and editPage, fixed selecting first page and fixed ordering the pages in menu

--- src/System/UI/loadPage.php
+++ src/System/UI/loadPage.php
@@ -2,6 +2,8 @@
 include_once("System/Business/Login/login.php");
 
 function elm_Page_Load(){
+    @session_start();
+
     global  $elm_Page_HTML;
     global $elm_Page_Content;
 
@@ -48,11 +50,10 @@ function elm_Page_LoginFunctionality(){
 
 function elm_Page_CreateMenu(){
     global $elm_Page_Content;
-
     $menuContent = '';
     foreach (elm_Page_GetAllPages() as $page){
         $menuContent = $menuContent . '<a href="index.php?page='. $page->id . '"';
-        if($page->id == $_SESSION['elm_Pages_CurrentPageId']){
+        if($page->id == (string)$_SESSION['elm_Pages_CurrentPageId']){
             $menuContent = $menuContent . 'class="active"';
             $elm_Page_Content = $page->content;
         }
@@ -61,32 +62,38 @@ function elm_Page_CreateMenu(){
     elm_Page_ReplacePlaceholder("[elm_PageNav]", $menuContent);
 }
 
-
 function elm_Page_GetAllPages(){
     $pages = elm_Data_GetPages();
 
-    //Add Admin Pages here
+    //Adds Admin Page
+    $adminpage = new elm_Page();
+    $adminpage -> id = 'elm_Admin';
+    $adminpage -> content = file_get_contents('System/UI/HTML/adminPage.html', FILE_USE_INCLUDE_PATH);
+    $adminpage -> name = 'Admin';
+    $adminpage -> sorting = 9900;
+    array_push($pages, $adminpage);
 
+    //Adds Edit Page
+    $editPage = new elm_Page();
+    $editPage -> id = 'elm_Page_Edit';
+    $editPage -> content = file_get_contents('System/UI/HTML/editPage.html', FILE_USE_INCLUDE_PATH);
+    $editPage -> name = 'Seite bearbeiten';
+    $editPage -> parentPage = 'elm_Admin';
+    $editPage -> sorting = 9930;
+    array_push($pages, $editPage);
 
+    //Orders Array
+    usort($pages, function ($a, $b){return strcmp($a->sorting, $b->sorting);});
 
-    //Orders all pages by sorting
-    usort($pages, create_function('$a, $b', '
-        $a = $a->sorting;
-        $b = $b->sorting;
-
-        if ($a == $b) return 0;
-
-        $direction = strtolower(trim($direction));
-
-        return ($a < $b) ? -1 : 1;
-    '));
     return $pages;
 }
 
 function elm_Page_GetCurrentPageId(){
     if(isset($_SESSION['elm_Pages_CurrentPageId'])){
-        if(isset($_GET["page"]))
+        if(isset($_GET["page"])){
+            $_SESSION['elm_Pages_LastPageId'] = $_SESSION['elm_Pages_CurrentPageId'];
             $_SESSION['elm_Pages_CurrentPageId'] = $_GET["page"];
+        }
     }
     else
         $_SESSION['elm_Pages_CurrentPageId'] = 0;
