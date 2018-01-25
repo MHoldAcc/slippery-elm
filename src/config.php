eb030d6f1bacdaaa08e05fab3ef656361c893633
Console Argonaut <vanderheide.michael@gmail.com>
2018-01-25 20:08:27 +0100

    Created new release - Version 0.102

--- src/config.php
+++ src/config.php
@@ -2,7 +2,7 @@
 /**
  * This variable is used to determine to what database type one wants to connect
  */
-$elm_Settings_ConnectionHost = "pgsql";  // value for mariaDB is "mysql", value for PostgreSQL is "pgsql"
+$elm_Settings_ConnectionHost = "mysql";  // value for mariaDB is "mysql", value for PostgreSQL is "pgsql"
 
 /**
  * This variable is used to pass the location of the server, where the database is hosted, to the connection string
@@ -12,7 +12,7 @@ $elm_Settings_ConnectionString = "localhost";
 /**
  * This variable is used to tell the connection with which user to connect
  */
-$elm_Settings_DbUser = "postgres"; //root
+$elm_Settings_DbUser = "root"; //root
 
 /**
  * This variable is used to tell the connection to which port to connect
@@ -24,7 +24,7 @@ $elm_Settings_DbUser = "postgres"; //root
 /**
  * This variable is used to tell the connection with which password the user uses identify himself to the server and the database
  */
-$elm_Settings_DbPassword = "1234"; // ""
+$elm_Settings_DbPassword = ""; // ""
 
 /**
  * This variable tells the connection string to which database to use
--- src/version.php
+++ src/version.php
@@ -1,4 +1,4 @@
 <?php
-$elm_Version_Number = "00.001";
-$elm_Version_Notes = "No specific Updates";
+$elm_Version_Number = "00.102";
+$elm_Version_Notes = "First installable Version";
 ?>
