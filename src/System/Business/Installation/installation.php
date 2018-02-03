a9ffef580316a25a329387c91af0e6777d896fbe
Console Argonaut <vanderheide.michael@gmail.com>
2018-02-03 20:08:06 +0100

    Added object oriented to installation.php

--- src/System/Business/Installation/installation.php
+++ src/System/Business/Installation/installation.php
@@ -1,39 +1,41 @@
 <?php
 
-//Variable to check if the user got created
-$elm_Installation_UserGotCreated = false;
-
-//If URL = index.php?install_db and database is non existing -> Initialize database
-if(isset($_GET['install_db']) && !elm_Data_GetIsDbInitialized()){
-    elm_Data_InitializeDb();
-    //Refresh page after initializing db
-    header("Refresh:0");
-}
-//If admin data is given in post -> Create admin user
-else if(isset($_POST['elm_Post_Username']) && isset($_POST['elm_Post_Password']) && isset($_POST['elm_Post_Email'])){
-    //Stripslashes from variables so there can't be any sql injections
-    $elm_Post_Username = stripslashes($_POST['elm_Post_Username']);
-    $elm_Post_Password = stripslashes($_POST['elm_Post_Password']);
-    $elm_Post_Email = stripslashes($_POST['elm_Post_Email']);
-
-    //Check if all variables have a value
-    if($elm_Post_Username != '' && $elm_Post_Password != '' &&  $elm_Post_Email != ''){
-        //Create admin user
-        if(elm_Data_CreateUser($elm_Post_Username, $elm_Post_Password, $elm_Post_Email, elm_Data_GetRoleId('admin')))
-            $elm_Installation_UserGotCreated = true;
+class elm_Installation {
+    /**
+     * Initializes slippery elm database
+     * @return bool True if db got initialized
+     */
+    public static function initializeDb() : bool {
+        //Only execute if db wasn't already created
+        if(!elm_Data_GetIsDbInitialized()){
+            elm_Data_InitializeDb();
+            return true;
+        }
+        return false;
     }
-}
 
-//Show the correct content depending on database state
-if($elm_Installation_UserGotCreated) {
-    //Refreshes page so page content can gets loaded
-    header("Refresh:0");
-}
-else if(elm_Data_GetIsDbInitialized()){
-    include_once("System/UI/HTML/adminCreation.html");
-}
-else {
-    include_once("System/UI/HTML/installation.html");
+    /**
+     * Creates an admin user with given credentials
+     * @param string $username The username for the admin user
+     * @param string $password The password for the admin user
+     * @param string $email The email for the admin user
+     * @return bool True if creation of the admin user was successful
+     */
+    public static function createAdminUser(string $username, string $password, string $email) : bool {
+        //Stripslashes from variables so there can't be any sql injections
+        $username = stripslashes($username);
+        $password = stripslashes($password);
+        $email = stripslashes($email);
+
+        //Check if all variables have a value
+        if($username != '' && $password != '' &&  $email != ''){
+            //Create admin user
+            if(elm_Data_CreateUser($username, $password, $email, elm_Data_GetRoleId('admin')))
+                return true;
+        }
+
+        return false;
+    }
 }
 
 ?>
--- src/System/default.php
+++ src/System/default.php
@@ -5,14 +5,26 @@ include_once("config.php");
 include_once("version.php");
 include_once("System/Business/Objects/objects.php");
 include_once("System/Database/data.php");
+include_once("System/Business/Installation/installation.php");
 
 //Check if installation was already done
 if(!elm_Data_GetIsDbInitialized()){
-    include_once("System/Business/Installation/installation.php");
+    if(isset($_GET['install_db'])){
+        elm_Installation::doInstallation();
+        header("Refresh:0");
+    }
+    else
+        include_once("System/UI/HTML/installation.html");
 }
 //If installation was done but no admin user got created yet:
 else if (count(elm_Data_GetUsers()) === 0){
-    include_once("System/Business/Installation/installation.php");
+    //If admin data is given in post -> Create admin user
+    if(isset($_POST['elm_Post_Username']) && isset($_POST['elm_Post_Password']) && isset($_POST['elm_Post_Email'])){
+        elm_Installation::createAdminUser($_POST['elm_Post_Username'], $_POST['elm_Post_Password'], $_POST['elm_Post_Email']);
+        header("Refresh:0");
+    }
+    else
+        include_once("System/UI/HTML/installation.html");
 }
 //If installation was completed:
 else{
