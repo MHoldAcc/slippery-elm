a3255135bca8d3c6e3bfbf1ceb8deaa8ba907b8c
Michael van der Heide <vanderheide.michael@gmail.com>
2018-04-26 17:14:00 +0200

    Removed everything for a fresh start

--- src/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-include_once("System/default.php");
-$elm_Main = new elm_Main();
-$elm_Main->start();
-?>
--- src/version.php
+++
@@ -1,4 +0,0 @@
-<?php
-$elm_Version_Number = "00.102";
-$elm_Version_Notes = "First installable Version";
-?>
